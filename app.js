88ca284d2399bc8e6d33cc6fb5ec5e1fede77a17
Naseem-gif <nk842932@gmail.com>
2025-12-16 21:53:05 +0500

    updated

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector(".rest-btn");
-
-
+let resetBtn = document.querySelector("#reset-btn");
+let msgContainer = document.querySelector(".msg-container");
+let newGameBtn = document.querySelector("#new-btn")
 
 let turnO = true;
-
-
 
 const winPatterns = [
   [0, 1, 2],
@@ -19,32 +17,72 @@ const winPatterns = [
 ];
 
 
+const resetGame = () => {
+    turnO = true;
+    enableBtns();
+    msgContainer.classList.add("hide")
+
+}
 
 
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        if(turnO){
-            console.log("btn was clicked")
-            box.innerText = "O"
-            turnO = false
-        }else{
-            box.innerText = "X"
-            turnO = true
-        }
-
+const disabledBtns = () => {
+    for(let box of boxes){
         box.disabled = true
-
-        checkWinner()
-    })
-
-    
-})
-
-
-const checkWinner = () => {
-    for(let pattern of winPatterns) {
-        console.log(pattern[0], pattern[1], pattern[2])
+        
     }
 }
 
 
+
+const enableBtns = () => {
+    for(let box of boxes) {
+        box.disabled = false
+        box.innerText = ""
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations the Winner is  ${winner}`
+    msgContainer.classList.remove("hide");
+    disabledBtns()
+}
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("btn was clicked");
+    if (turnO) {
+      
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("Winner ", pos1Val);
+        showWinner(pos1Val)
+      }
+    }
+  }
+};
+
+
+
+
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
